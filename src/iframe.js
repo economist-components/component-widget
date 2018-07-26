5582c38104b3321115444716928cf4b4bc7d47f9
Peter Varholak <peter.varholak@gmail.com>
2018-07-26 13:23:53 +0200

    Revert "Revert "DISCO-516 pass id prop to iframe element""

--- src/iframe.js
+++ src/iframe.js
@@ -30,10 +30,11 @@ export default class Iframe extends React.Component {
   }
 
   render() {
-    const { src, width, height, className, scrollStatus } = this.props;
+    const { src, width, height, className, scrollStatus, id } = this.props;
     return (
       <iframe
         src={src}
+        id={id}
         width={width}
         height={height}
         className={classNames('widget-iframe', className)}
@@ -56,5 +57,6 @@ if (process.env.NODE_ENV !== 'production') {
     height: PropTypes.string,
     className: PropTypes.string,
     scrollStatus: PropTypes.string,
+    id: PropTypes.string,
   };
 }
